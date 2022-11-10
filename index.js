de284c7f11385f72e737a3f64be3603c33aafeec
Richy23456 <115689692+Richy23456@users.noreply.github.com>
2022-11-10 09:54:23 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var x = myFunction(4, 3);
+document.getElementById("demo").innerHTML = ;
+"Prima addizioene in js " + myFunction(a, b);
+function myFunction(a, b) {
+  return a + b;
+}
+
+var y = myFunction(7, 9);
+document.getElementById("demo").innerHTML = 
+"Prima moltiplicazione in js "+ MyFunction(a, b); 
+function MyFunction(a, b) {
+  return a * b;
+}
